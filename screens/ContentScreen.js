e36021cf84fe57f1620a99761aac279563f254b0
Shashank <shashankr8724@gmail.com>
2023-08-05 22:55:55 +0530

    Added Filters
    Filters for different asanas has been added

--- screens/ContentScreen.js
+++ screens/ContentScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, ScrollView, TouchableOpacity, Modal, Pressable } from 'react-native'
+import { View, Text, Image, ScrollView, TouchableOpacity, Modal, Pressable, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import * as NavigationBar from 'expo-navigation-bar';
@@ -9,6 +9,7 @@ import { auth } from '../firebase';
 import AsanaCard from '../components/AsanaCard';
 import AsanaCardLarge from '../components/AsanaCardLarge';
 import { StatusBar } from 'expo-status-bar';
+import { FlashList } from '@shopify/flash-list';
 
 const ContentScreen = () => {
     NavigationBar.setVisibilityAsync("hidden");
@@ -41,13 +42,14 @@ const ContentScreen = () => {
     const [loading, setLoading] = useState(true);
     const [poses, setPoses] = useState([]);
     const poseNames = async () => {
+        setLoading(true)
         await fetch("https://pqxc11fj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22asana%22%5D%7B%0A++_id%2C+en_name%2C+imgUrl%0A%7D")
             .then((response) => response.json())
             .then((result) => {
                 setPoses(result.result)
                 setTimeout(() => {
                     setLoading(false)
-                }, 2000)
+                }, 1000)
             })
     }
 
@@ -69,9 +71,61 @@ const ContentScreen = () => {
             })
     }
 
+    const [showFilter, setshowFilter] = useState(false)
+
+    const [showType, setshowType] = useState(false)
+    const [typeData, setTypeData] = useState([]);
+    const getTypeData = async () => {
+        await fetch("https://pqxc11fj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22poses_type%22%5D%7B%0A++_id%2C+typeName%0A%7D%0A")
+            .then((response) => response.json())
+            .then((result) => {
+                setTypeData(result.result)
+            })
+    }
+
+    const [showAnatomy, setshowAnatomy] = useState(false)
+    const [anatomyData, setanatomyData] = useState([]);
+    const getanatomyData = async () => {
+        await fetch("https://pqxc11fj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22poses_anatomy%22%5D%7B%0A++_id%2C+anatomyName%0A%7D%0A")
+            .then((response) => response.json())
+            .then((result) => {
+                setanatomyData(result.result)
+            })
+    }
+
+    const [filterSelected, setfilterSelected] = useState(false)
+    const [filteredData, setfilteredData] = useState([])
+    const getFilteredData = async (id, filterType) => {
+        setLoading(true)
+        if (filterType == "type") {
+            await fetch(`https://pqxc11fj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22poses_type%22+%26%26+_id%3D%3D%22${id}%22%5D%7B%0A++_id%2C+typeName%2C%0A++asanas%5B%5D-%3E%7B_id%2Cen_name%2C+imgUrl%7D%0A%7D`)
+                .then((response) => response.json())
+                .then((result) => {
+                    setfilteredData(result.result[0].asanas);
+                    setfilterSelected(true);
+                    setTimeout(() => {
+                        setLoading(false);
+                    }, 1000)
+                })
+        } else if (filterType == "anatomy") {
+            await fetch(`https://pqxc11fj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22poses_anatomy%22+%26%26+_id%3D%3D%22${id}%22%5D%7B%0A++_id%2C+anatomyName%2C%0A++asanas%5B%5D-%3E%7B_id%2Cen_name%2C+imgUrl%7D%0A%7D`)
+                .then((response) => response.json())
+                .then((result) => {
+                    setfilteredData(result.result[0].asanas);
+                    setfilterSelected(true);
+                    setTimeout(() => {
+                        setLoading(false);
+                    }, 1000)
+                })
+        }
+
+    }
+
     useEffect(() => {
         getUser();
         poseNames();
+        getTypeData();
+        getanatomyData();
     }, [])
 
     return (
@@ -87,39 +141,58 @@ const ContentScreen = () => {
                         />
                     </View>
                     :
-                    <View style={{ gap: 30, width: "100%" }} className="flex-1 items-start justify-start">
+                    <View style={{ gap: 30, width: "100%", height: "100%" }} className="flex items-start justify-start">
                         <View style={{ gap: -5 }} className="flex items-start justify-start">
                             <Text style={{ fontFamily: "PoppinsLightItalic", fontSize: 20 }}>Hello</Text>
                             <Text style={{ fontFamily: "PoppinsBold", fontSize: 28 }}>{userdisplayName}!</Text>
                         </View>
-                        <View style={{ gap: 10, height: "85%", width: "100%" }} className="flex-col justify-start items-start">
-                            <Text style={{ fontFamily: "PoppinsRegular", fontSize: 14 }} className="text-[#565656]">Yoga Asanas</Text>
-                            <ScrollView style={{ width: "100%" }} showsVerticalScrollIndicator={false} contentContainerStyle={{ gap: 10, alignItems: 'center', width: "100%" }} className="rounded-[10px]">
-                                {loading ?
-                                    <View style={{ width: "100%", height: "100%" }} className="flex items-center justify-center">
-                                        <LottieView
-                                            style={{ width: 100, height: 100 }}
-                                            autoPlay
-                                            source={require("../assets/lottie/newScene.json")}
-                                        />
-                                    </View>
-                                    :
-                                    poses.map(pose =>
-                                        // <AsanaCardLarge key={pose._id} id={pose._id} title={pose.en_name} img={pose.imgUrl} />
-                                        <TouchableOpacity key={pose._id} activeOpacity={0.8} onPress={() => { getId(pose._id) }} style={{ width: "100%", height: 150 }} className="bg-[#000] flex-col justify-end items-start rounded-[10px]" >
-                                            <Image className="rounded-[10px]" style={{ width: "100%", height: 150, position: 'absolute', top: 0, right: 0, overflow: 'hidden' }} resizeMode='center' source={{ uri: pose.imgUrl }} />
-                                            <View style={{ width: "100%", height: 150, paddingHorizontal: 20, paddingVertical: 20 }} className="bg-[#00000047] flex-col justify-end items-start rounded-[10px]">
-                                                {
-                                                    pose.en_name.length > 24 ?
-                                                        <Text className="color-white" style={{ fontFamily: "PoppinsMedium", fontSize: 18, textAlign: "left" }}>{pose.en_name.slice(0, 24)} ...</Text>
-                                                        :
-                                                        <Text className="color-white" style={{ fontFamily: "PoppinsMedium", fontSize: 18, textAlign: "left" }}>{pose.en_name}</Text>
-                                                }
+                        <View style={{ gap: 20, height: "85%", width: "100%" }} className="flex-col justify-start items-start">
+                            <View style={{ width: "100%" }} className="flex-row justify-between items-center">
+                                <Text style={{ fontFamily: "PoppinsRegular", fontSize: 14 }} className="text-[#565656]">Available Yoga Asanas</Text>
+                                <Pressable onPress={() => { setshowFilter(true) }} style={{ width: 42, height: 42 }} className="flex rounded-full items-center justify-center">
+                                    <Image source={require("../assets/icons/filter.png")} />
+                                </Pressable>
+                            </View>
+                            {loading ?
+                                <View style={{ width: "100%", height: "100%" }} className="flex items-center justify-center">
+                                    <LottieView
+                                        style={{ width: 100, height: 100 }}
+                                        autoPlay
+                                        source={require("../assets/lottie/newScene.json")}
+                                    />
+                                </View>
+                                :
+                                <View style={{ height: "90%", paddingLeft: 20, width: "100%" }}>
+                                    {
+                                        filteredData == null ?
+                                            <View style={{ marginLeft: -20 }} className="flex-1 justify-center items-center">
+                                                <Image style={{ width: 250, height: 250 }} source={require("../assets/icons/nothing_found.png")} />
+                                                <Text style={{ fontFamily: "PoppinsBold", fontSize: 48 }} className="text-[#565656]">Oops!</Text>
+                                                <Text style={{ fontFamily: "PoppinsMedium", fontSize: 20 }} className="text-[#565656]">Nothing here</Text>
                                             </View>
-                                        </TouchableOpacity >
-                                    )
-                                }
-                            </ScrollView>
+                                            :
+                                            <FlashList
+                                                numColumns={2}
+                                                showsVerticalScrollIndicator={false}
+                                                data={filterSelected ? filteredData : poses}
+                                                renderItem={({ item }) =>
+                                                    <TouchableOpacity activeOpacity={0.5} onPress={() => { getId(item._id) }} className="bg-white rounded-xl" style={{ marginBottom: 20, width: 150, height: 150 }}>
+                                                        <Image className="absolute rounded-xl" style={{ width: 150, height: 150 }} resizeMode='cover' source={{ uri: item.imgUrl }} />
+                                                        <View className="bg-[#0000003e] rounded-xl flex items-start justify-end" style={{ padding: 20, marginBottom: 20, width: 150, height: 150 }}>
+                                                            {
+                                                                item.en_name.length > 13 ?
+                                                                    <Text className="color-white" style={{ fontFamily: "PoppinsBold", fontSize: 18, textAlign: "left" }}>{item.en_name.slice(0, 13)} ...</Text>
+                                                                    :
+                                                                    <Text className="color-white" style={{ fontFamily: "PoppinsBold", fontSize: 18, textAlign: "left" }}>{item.en_name}</Text>
+                                                            }
+                                                        </View>
+                                                    </TouchableOpacity>
+                                                }
+                                                estimatedItemSize={(filterSelected ? filteredData : poses).length}
+                                            />
+                                    }
+                                </View>
+                            }
                         </View>
                     </View>
             }
@@ -164,19 +237,94 @@ const ContentScreen = () => {
                                         <Text style={{ fontFamily: "PoppinsRegular", fontSize: 16, textAlign: 'left' }}>Step by step instructions</Text>
                                         {
                                             poseData.steps ?
-                                            poseData.steps.map(step =>
-                                                <View key={poseData.steps.indexOf(step)} style={{ maxWidth: "95%" }} className="flex-row justify-start items-start">
-                                                    <Text className="text-[#565656]" style={{ fontFamily: "PoppinsRegular", fontSize: 14 }}>{poseData.steps.indexOf(step)+1}. </Text>
-                                                    <Text className="text-[#565656]" style={{ fontFamily: "PoppinsRegular", fontSize: 14 }}>{step}</Text>
-                                                </View>
-                                            )
-                                            :
-                                            <View></View>
+                                                poseData.steps.map(step =>
+                                                    <View key={poseData.steps.indexOf(step)} style={{ maxWidth: "95%" }} className="flex-row justify-start items-start">
+                                                        <Text className="text-[#565656]" style={{ fontFamily: "PoppinsRegular", fontSize: 14 }}>{poseData.steps.indexOf(step) + 1}. </Text>
+                                                        <Text className="text-[#565656]" style={{ fontFamily: "PoppinsRegular", fontSize: 14 }}>{step}</Text>
+                                                    </View>
+                                                )
+                                                :
+                                                <View></View>
                                         }
                                     </View>
                                 </ScrollView>
                             </View>
                     }
+                </View>
+            </Modal>
+            <Modal
+                visible={showFilter}
+                onRequestClose={() => { setshowFilter(false) }}
+                statusBarTranslucent={true}
+                transparent={true}>
+                <View style={{ padding: 25 }} className="flex-1 bg-[#00000028] justify-center items-center">
+                    <View style={{ width: "100%", padding: 20, gap: 10, maxHeight: "80%" }} className="flex bg-white rounded-[10px] justify-center items-start">
+                        <View style={{ maxHeight: "100%" }}>
+                            <Pressable onPress={() => { setshowAnatomy(false); setshowType(false); setfilterSelected(false); setshowFilter(false); setfilteredData({}) }} style={{ width: "100%" }} className="flex-row justify-between items-center">
+                                <Text style={{ fontFamily: "PoppinsRegular", fontSize: 14, paddingTop: 3 }} className="text-[#565656]">All</Text>
+                            </Pressable>
+                            <Pressable onPress={() => { setshowType(!showType); setshowAnatomy(false) }} style={{ width: "100%" }} className="flex-row justify-between items-center">
+                                <Text style={{ fontFamily: "PoppinsRegular", fontSize: 14, paddingTop: 3 }} className="text-[#565656]">Poses by Type</Text>
+                                {
+                                    showType ?
+                                        <Image source={require("../assets/icons/arrowUp.png")} />
+                                        :
+                                        <Image source={require("../assets/icons/arrowDown.png")} />
+                                }
+                            </Pressable>
+                            {
+                                showType ?
+                                    <View style={{ maxHeight: "100%", width: "100%" }}>
+                                        <FlatList
+                                            showsVerticalScrollIndicator={false}
+                                            data={typeData}
+                                            numColumns={2}
+                                            contentContainerStyle={{
+                                                padding: 10
+                                            }}
+                                            renderItem={({ item }) =>
+                                                <Pressable onPress={() => { getFilteredData(item._id, "type"); setshowFilter(false); setshowType(false); setshowAnatomy(false) }} style={{ marginRight: 20, width: "45%", padding: 3 }}>
+                                                    <Text key={item._id} style={{ fontFamily: "PoppinsRegular", fontSize: 14, paddingTop: 3 }} className="text-[#565656]">{item.typeName}</Text>
+                                                </Pressable>
+                                            }
+                                            estimatedItemSize={typeData.length}
+                                        />
+                                    </View>
+                                    :
+                                    <View></View>
+                            }
+                            <Pressable onPress={() => { setshowAnatomy(!showAnatomy) }} style={{ width: "100%" }} className="flex-row justify-between items-center">
+                                <Text style={{ fontFamily: "PoppinsRegular", fontSize: 14, paddingTop: 3 }} className="text-[#565656]">Poses by Anatomy</Text>
+                                {
+                                    showAnatomy ?
+                                        <Image source={require("../assets/icons/arrowUp.png")} />
+                                        :
+                                        <Image source={require("../assets/icons/arrowDown.png")} />
+                                }
+                            </Pressable>
+                            {
+                                showAnatomy ?
+                                    <View style={{ maxHeight: "100%", width: "100%" }}>
+                                        <FlatList
+                                            showsVerticalScrollIndicator={false}
+                                            data={anatomyData}
+                                            numColumns={2}
+                                            contentContainerStyle={{
+                                                padding: 10
+                                            }}
+                                            renderItem={({ item }) =>
+                                                <Pressable onPress={() => { getFilteredData(item._id, "anatomy"); setshowType(false); setshowAnatomy(false); setshowFilter(false); }} style={{ marginRight: 20, width: "45%", padding: 3 }}>
+                                                    <Text key={item._id} style={{ fontFamily: "PoppinsRegular", fontSize: 14, paddingTop: 3 }} className="text-[#565656]">{item.anatomyName}</Text>
+                                                </Pressable>
+                                            }
+                                            estimatedItemSize={anatomyData.length}
+                                        />
+                                    </View>
+                                    :
+                                    <View></View>
+                            }
+                        </View>
+                    </View>
                 </View>
             </Modal>
             <StatusBar style='dark' />
@@ -185,3 +333,10 @@ const ContentScreen = () => {
 }
 
 export default ContentScreen
+
+//Get Asana details for specific filter
+
+// *[_type=="poses_type" && _id=="33358354-bcc9-4eb5-8f86-d0fc9ee8e6c2"]{
+//     _id, typeName,
+//     asanas[]->
+//   }
